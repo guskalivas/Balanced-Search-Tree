bc78abc0479a289e02e50a22f502adf15ca955d4
Gus Kalivas <56047433+guskalivas@users.noreply.github.com>
2020-04-23 16:08:20 -0500

    Update BSTNode.java

--- BSTNode.java
+++ BSTNode.java
@@ -1,18 +1,4 @@
-// Students may use and edit this class as they choose
-// Students may add or remove or edit fields, methods, constructors for this class
-// Students may inherit from an use this class in any way internally in other classes.
-// Students are not required to use this class. 
-// BUT, IF YOUR CODE USES THIS CLASS, BE SURE TO SUBMIT THIS CLASS
-//
-// RECOMMENDED: do not use public or private visibility modifiers
-// and make this an inner class in your tree implementation.
-//
-// package level access means that all classes in the package can access directly.
-// and accessing the node fields from classes other than your balanced search 
-// is bad design as it creates many more chances for bugs to be introduced and not
-// caught.
-//
-// Classes that use this type:  <TODO, list which if any classes use this type>
+
 class BSTNode<K,V> {
     
     K key;
